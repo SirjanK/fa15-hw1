2f0af68d5bd60780a8615cb7c3dfb5964774a1f8
Sirjan Kafle <sxkafle12@gmail.com>
2016-02-12 11:55:02 -0800

    Finished first homework

--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,19 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  (a + b) * (a + b)
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+    a.sort.map{ |b| b += 1}
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+    "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
+  def baz(arr)
+    arr.map{|e| e.to_i}.map{|e| e+=2}.select{|e| e%2 == 0 and e<=10}.reduce(0, :+)
+  end
   # DO NOT EDIT THIS CODE BELOW
   require './foobar'
   Foobar.baz a
